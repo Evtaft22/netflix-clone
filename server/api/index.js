93cc71853141130884dd79a0b43a7628ffca9e49
Evtaft22 <evtaft22@gmail.com>
2024-01-05 19:17:07 -0600

    Setup middleware routes

---
+++ server/api/index.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const { itemsRouter } = require("./items");
+
+const apiRouter = Router();
+
+apiRouter.use("/items", itemsRouter);
+
+module.exports = {
+  apiRouter,
+};
