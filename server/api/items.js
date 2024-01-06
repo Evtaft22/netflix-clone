33cb1eee3fb90bae11c4b313418f23803e553f60
Evtaft22 <evtaft22@gmail.com>
2024-01-05 19:17:33 -0600

    Establish routes for items

---
+++ server/api/items.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const db = require("../db");
+
+const itemsRouter = Router();
+
+// TODO: Your routes here!
+
+// itemsRouter.get('/', (req, res) => {
+// });
+
+module.exports = {
+  itemsRouter,
+};
