006d735018d5e06053bfe592b4ef5afb657e6879
Evtaft22 <evtaft22@gmail.com>
2024-01-05 19:19:52 -0600

    Create basic components for list and items

---
+++ client/src/components/ListItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ListItem = ({ item }) => <li>{item.description}</li>;
+
+ListItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default ListItem;
