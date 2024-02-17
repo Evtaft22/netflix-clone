5f56068545957128548bd022ded78833c6da1683
Evtaft22 <evtaft22@gmail.com>
2024-02-17 13:18:38 -0600

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
+const path = require('path');
 
-const SRC_DIR = path.join(__dirname, "/client/src");
-const DIST_DIR = path.join(__dirname, "/client/dist");
-const IS_PROD = process.env.NODE_ENV === "production";
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+const IS_PROD = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: DIST_DIR,
   },
-  devtool: IS_PROD ? "cheap-source-map" : "eval-source-map",
+  devtool: IS_PROD ? 'cheap-source-map' : 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
     ],
   },
