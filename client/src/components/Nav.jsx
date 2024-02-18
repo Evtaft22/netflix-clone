49635ba4180c0cbf359c08af124ec9ce5b63aeff
Evtaft22 <evtaft22@gmail.com>
2024-02-17 22:15:20 -0600

    Add event listener to add background to navbar when scrolling down in the app

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Nav.css';
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && 'nav__black'}`}>
       <img
         className="nav__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
         alt="Netflix Logo"
       />
 
       <img
         className="nav__avatar"
-        src="https://pbs.twimg.com/profile_images/124011999041155"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
         alt="Netflix Logo"
       />
     </div>
