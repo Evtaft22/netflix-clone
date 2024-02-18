a9d7f307f2d3664f38a87175bac60bdaef34e1d7
Evtaft22 <evtaft22@gmail.com>
2024-02-17 19:27:12 -0600

    Add banner contents, truncate functionality, and import styling

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../services/axios';
 import requests from '../services/requests';
+import '../styles/Banner.css';
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -19,6 +20,10 @@ function Banner() {
 
   console.log('movie: ', movie);
 
+  function truncate(str, n) {
+    return str?.length > n ? `${str.substr(0, n - 1)}...` : str;
+  }
+
   return (
     <header
       className="banner"
@@ -29,7 +34,14 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
       </div>
       {/* div > 2 buttons */}
       {/* description */}
