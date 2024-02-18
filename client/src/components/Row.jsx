a47e9649d7dc627da1d8c652512817060fab6118
Evtaft22 <evtaft22@gmail.com>
2024-02-17 18:11:54 -0600

    List movie posters in Row component

--- client/src/components/Row.jsx
+++ client/src/components/Row.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../services/axios'; // the variable "instance" was exported as default, so we rename it when we import it here
+import '../styles/Row.css';
+
+const baseUrl = 'https://image.tmdb.org/t/p/original';
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -8,17 +11,27 @@ function Row({ title, fetchUrl }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log('request: ', request);
-      return request;
+      setMovies(request.data.results);
+      return request.data.results;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+  console.log('movies: ', movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
-
-      {/* container -> posters */}
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
