76caad931bee969adcbf18de3b618f3b7f4106b4
Evtaft22 <evtaft22@gmail.com>
2024-02-20 16:05:51 -0600

    Fix netflix originals api url

--- client/src/services/requests.js
+++ client/src/services/requests.js
@@ -1,8 +1,9 @@
 const API_KEY = 'cadd8508a1c23cd8b4ece3435075f8f1';
 
 const requests = {
+  // fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=primary_release_date.desc&with_companies=178464`,
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
